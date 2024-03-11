487dbe160f91a2572a403d15be20609578b6d200
Krish_Raziel19 <112201013@smail.iitpkd.ac.in>
2024-03-12 00:15:38 +0530

    Used pandas to extract input from xl sheet

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Development-of-suitable-program-for-dispaying-the-drug-conecentration-in-a-sensor-device
 This is the github repository for the Open Ended Lab Project (OELP) titled Development-of-suitable-program-for-dispaying-the-drug-conecentration-in-a-sensor-device. The details of this project are as follows:
-1. Faculty mentor for the project: Dr.Abdul Rasheed P
+1. Faculty mentor for the project: Dr. Abdul Rasheed P
 2. Students who've taken the project:
     a. Mr. Bhogaraju Shanmukha Sri Krishna
     b. Mr. Darla Manohar
---
+++ test.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+'''
+    All the codes used in this file can be found at the following webpage
+    https://www.digitalocean.com/community/tutorials/pandas-read_excel-reading-excel-file-in-python
+    
+    Here, pandas, a popular python library is used to read inputs from an excel sheet.
+    After reading the input, it also gets the data specified in the coloumns to a hashmap.
+'''
+
+df = 0 # random init to avoid errors
+df = pd.read_excel('../Cities_Probability(1).xlsx') # reads the entire sheet (by default the first sheet)
+
+
+''' prints the entire sheet '''
+# print(df) 
+
+''' prints the coloumns '''
+# print(df.columns.ravel()) 
+
+''' gets a list representation of the elements in the specified coloumn '''
+# print(df['population'].tolist())
+
+input_data = {}
+avg_values = {}
+for col_name in df.columns.ravel()[1:]:
+    input_data[col_name] = df[col_name].tolist()
+    
+    # initializing each value in avg_values
+    avg_values[col_name] = 0
+
+numeric = list(input_data.keys())[1:]
+
+for col_name in numeric:
+    for i in input_data[col_name]:
+        avg_values[col_name] += int(i)
+    avg_values[col_name] = avg_values[col_name] / len(input_data[col_name])
+
+print(avg_values.values())
+
+''' calculating the average of each coloumn as an exercise '''
+
+
+
+# print(input_data)
